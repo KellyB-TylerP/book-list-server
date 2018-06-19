87760e891dca75f94b568a4cc76736862cb7925e
Kelly Bennett <cornerkick87@gmail.com>
2018-06-18 17:05:20 -0700

    updated server.js

--- server.js
+++ server.js
@@ -10,18 +10,27 @@ const app = express();
 const PORT = process.env.PORT;
 
 //Database setup
+const conString = 'postgres://localhost:5432/books_app';
+const client = new pg.Client(conString);
+client.connect();
+client.on('error', err => console.log(err));
 
 //application middleware
 app.use(cors());
 
 //API Endpoints
-app.get('/api/v1/test', (req, res) => {
-    console.log('OMG i have been visited by a client')
-    res.send('OMG I am in contact with the server!!!!');
+app.get('/api/v1/books', (req, res) => {
+    console.log('OMG i handling a git request by a client');
+    let SQL = 'SELECT * FROM books;';
+    client.query(SQL)
+        .then(results => res.send(results.rows))
+        .catch(console.log.error);
 });
 
 app.get('*', (req, res) => res.status(404).send('This route does not exist'));
 
 app.listen(PORT, () => console.log(`The server is alive and well and listening on port ${PORT}`));
+
+
 
 
